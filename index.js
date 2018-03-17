9694ba02cd09bb8fea38356c3034643b16a86684
2534290808 <2534290808@qq.com>
2018-03-17 22:38:01 +0800

    export DefaultHeader
    Signed-off-by: 2534290808 <2534290808@qq.com>

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 export {default as SmartRefreshControl} from './SmartRefreshControl';
 export {default as ClassicsHeader} from './ClassicsHeader';
-export {default as StoreHouseHeader} from './StoreHouseHeader'
+export {default as StoreHouseHeader} from './StoreHouseHeader';
+export {default as DefaultHeader} from './DefaultHeader';
