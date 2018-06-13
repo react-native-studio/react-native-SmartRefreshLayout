0f613c40b0a1d118205d715d9ce81e14fa961bb9
2534290808 <2534290808@qq.com>
2018-06-13 20:39:13 +0800

    add primaryColor属性

--- android/src/main/java/com/lmy/header/AnyHeaderManager.java
+++ android/src/main/java/com/lmy/header/AnyHeaderManager.java
@@ -1,7 +1,11 @@
 package com.lmy.header;
 
+import android.graphics.Color;
+import android.support.annotation.ColorInt;
+
 import com.facebook.react.uimanager.ThemedReactContext;
 import com.facebook.react.uimanager.ViewGroupManager;
+import com.facebook.react.uimanager.annotations.ReactProp;
 
 /**
  * Created by painter.g on 2018/3/12.
@@ -16,5 +20,17 @@ public class AnyHeaderManager extends ViewGroupManager<AnyHeader> {
     @Override
     protected AnyHeader createViewInstance(ThemedReactContext reactContext) {
         return new AnyHeader(reactContext);
+    }
+
+    /**
+     * 设置主调色
+     * @param view
+     * @param primaryColor
+     */
+    @ReactProp(name = "primaryColor")
+    public void setPrimaryColor(AnyHeader view, String primaryColor){
+        if(primaryColor!=null && !"".equals(primaryColor)){
+            view.setPrimaryColor(Color.parseColor(primaryColor));
+        }
     }
 }
