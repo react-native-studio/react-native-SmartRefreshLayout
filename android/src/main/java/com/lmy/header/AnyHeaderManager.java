7a6eb2ba2012b901ea7e71454058e8f412a97fc9
2534290808 <2534290808@qq.com>
2018-06-14 10:05:40 +0800

    add spinnerStyle属性

--- android/src/main/java/com/lmy/header/AnyHeaderManager.java
+++ android/src/main/java/com/lmy/header/AnyHeaderManager.java
@@ -6,6 +6,10 @@ import android.support.annotation.ColorInt;
 import com.facebook.react.uimanager.ThemedReactContext;
 import com.facebook.react.uimanager.ViewGroupManager;
 import com.facebook.react.uimanager.annotations.ReactProp;
+import com.lmy.smartrefreshlayout.SpinnerStyleConstants;
+import com.scwang.smartrefresh.layout.constant.SpinnerStyle;
+
+import java.util.HashMap;
 
 /**
  * Created by painter.g on 2018/3/12.
@@ -32,5 +36,15 @@ public class AnyHeaderManager extends ViewGroupManager<AnyHeader> {
         if(primaryColor!=null && !"".equals(primaryColor)){
             view.setPrimaryColor(Color.parseColor(primaryColor));
         }
+    }
+
+    /**
+     * 设置spinnerStyle
+     * @param view
+     * @param spinnerStyle
+     */
+    @ReactProp(name = "spinnerStyle")
+    public void setSpinnerStyle(AnyHeader view,String spinnerStyle){
+        view.setSpinnerStyle(SpinnerStyleConstants.SpinnerStyleMap.get(spinnerStyle));
     }
 }
