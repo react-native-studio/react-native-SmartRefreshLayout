8a596028b65bad30af43fe1684f1e554b2b7aee6
2534290808 <2534290808@qq.com>
2018-06-19 11:01:38 +0800

    update index.js

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ export {default as SmartRefreshControl} from './SmartRefreshControl';
 export {default as ClassicsHeader} from './ClassicsHeader';
 export {default as StoreHouseHeader} from './StoreHouseHeader';
 export {default as DefaultHeader} from './DefaultHeader';
+export {default as AnyHeader} from './AnyHeader';
