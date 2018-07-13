2f5e360259d16ae78c8e0039982d279e864027bc
2534290808 <2534290808@qq.com>
2018-07-13 15:42:06 +0800

    update Example

--- Example/App.js
+++ Example/App.js
@@ -74,6 +74,7 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: '#F5FCFF',
+        height:1000
     },
     welcome: {
         fontSize: 20,
