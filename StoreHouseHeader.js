9e655e979769540ff3e91d46ee382198a5a79913
2534290808 <2534290808@qq.com>
2018-07-27 17:57:35 +0800

    refactor StoreHouseHeader code

--- StoreHouseHeader.js
+++ StoreHouseHeader.js
@@ -1,34 +1,20 @@
 import React, {Component} from 'react';
-import {
-    StyleSheet,
-    View,
-    Text,
-    requireNativeComponent,
-    findNodeHandle,
-    UIManager,
-} from 'react-native';
-import {ViewPropTypes,PropTypes} from "./Util";
+import {requireNativeComponent} from 'react-native';
+import {ViewPropTypes, PropTypes} from "./Util";
 
 const RCTStoreHouseHeader = requireNativeComponent('RCTStoreHouseHeader', RCTStoreHouseHeader);
 
-class StoreHouseHeader extends Component {
+export default class StoreHouseHeader extends Component {
+    static propTypes = {
+        textColor: PropTypes.string,
+        text: PropTypes.string,//暂时只支持英文
+        fontSize: PropTypes.number,
+        lineWidth: PropTypes.number,
+        dropHeight: PropTypes.number,
+        ...ViewPropTypes,
+    }
 
     render() {
-        return (
-            <RCTStoreHouseHeader
-                {...this.props}
-            />
-
-        )
+        return (<RCTStoreHouseHeader {...this.props}/>)
     }
 }
-
-StoreHouseHeader.propTypes = {
-    textColor:PropTypes.string,
-    text:PropTypes.string,//暂时只支持英文
-    fontSize:PropTypes.number,
-    lineWidth:PropTypes.number,
-    dropHeight:PropTypes.number,
-    ...ViewPropTypes,
-}
-export default StoreHouseHeader;
