3b167c147c12f7c57782dec3c6a147d0595dfcb7
2534290808 <2534290808@qq.com>
2018-08-22 17:34:07 +0800

    add lottie-react-native

--- Example/android/app/src/main/java/com/example/MainApplication.java
+++ Example/android/app/src/main/java/com/example/MainApplication.java
@@ -3,6 +3,7 @@ package com.example;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.airbnb.android.react.lottie.LottiePackage;
 import com.lmy.smartrefreshlayout.SmartRefreshLayoutPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new LottiePackage(),
             new SmartRefreshLayoutPackage(),
             new VectorIconsPackage()      );
     }
