c319d2f8a596d0b0b46f7e01f0c4c45b994bc91f
2534290808 <2534290808@qq.com>
2018-08-23 17:29:56 +0800

    update LottieRefreshControl

--- Example/LottieRefreshControl.js
+++ Example/LottieRefreshControl.js
@@ -12,11 +12,11 @@ import {
 import PropTypes from 'prop-types';
 import LottieView from 'lottie-react-native'
 import {SmartRefreshControl, AnyHeader} from 'react-native-smartrefreshlayout';
-import Icon from 'react-native-vector-icons/Ionicons';
-import {SkypeIndicator} from 'react-native-indicators';
 
-const AnimatedIcon = Animated.createAnimatedComponent(Icon);
 export default class LottieRefreshControl extends Component {
+    state = {
+        scale: new Animated.Value(0.1)
+    }
     _onRefresh = () => {
         let {onRefresh} = this.props;
         onRefresh && onRefresh();
@@ -24,17 +24,28 @@ export default class LottieRefreshControl extends Component {
     finishRefresh=(params)=>{
         this._refreshc && this._refreshc.finishRefresh(params)
     }
+    _onHeaderMoving=(event)=>{
+        this.state.scale.setValue(event.nativeEvent.percent);
+    }
     render() {
         return (
             <SmartRefreshControl
+                onHeaderMoving={this._onHeaderMoving}
                 style={{flex:1}}
                 ref={ref => this._refreshc = ref}
                 children={this.props.children}
                 onRefresh={this._onRefresh}
                 headerHeight={100}
                 HeaderComponent={
-                    <AnyHeader style={{height:100,justifyContent:'center',alignItems:'center'}}>
+                    <AnyHeader>
+                        <Animated.View style={{height:100,justifyContent:'center',alignItems:'center',transform: [{
+                            scale:this.state.scale.interpolate({
+                            inputRange: [0,1,2],
+                            outputRange: [0.1,1,1],
+                        })
+                        }]}}>
                         <LottieView style={{width:100,height:100}} hardwareAccelerationAndroid  autoPlay source={require('./loop.json')} speed={1.5} />
+                        </Animated.View>
                     </AnyHeader>
                 }
             />
